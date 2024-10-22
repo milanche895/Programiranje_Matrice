82752fd13d6f3ed624432f31cf2b536d081ada5d
milanche895 <milanstojanovic895@gmail.com>
2024-10-22 18:29:44 +0200

    simple matrix

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numbers = { { 1, 2, 3 }, { 4, 5, 6 } };
+            for (int i = 0; i < numbers.GetLength(0); i++) {
+                Console.Write("Row" +i+ ": ");
+                for (int j = 0; j < numbers.GetLength(1); j++) {
+                    Console.Write(numbers[i, j] + "");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
